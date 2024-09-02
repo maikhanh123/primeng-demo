1393faa507d5b0ddc653edb906b7de2cd9b4a4bb
MaiKhanh <maikhanh123@gmail.com>
2024-09-02 14:41:23 +0800

    update testing directive

--- src/app/components/angular-directives/angular-directives.component.ts
+++ src/app/components/angular-directives/angular-directives.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FallbackImageDirective } from 'src/app/shared/directives/fallback-image.directive';
 
 @Component({
   selector: 'app-angular-directives',
   standalone: true,
   imports: [
-    CommonModule,
+    CommonModule, FallbackImageDirective
   ],
   templateUrl: './angular-directives.component.html',
   styleUrl: './angular-directives.component.scss',
