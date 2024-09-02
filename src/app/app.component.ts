511182b5ab9e3f407c81e767efd0d3c6b0fb22b1
MaiKhanh <maikhanh123@gmail.com>
2024-09-02 16:18:32 +0800

    Update angular service component path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { AngularDirectivesComponent } from "./components/angular-directives/angular-directives.component";
 import { AngularPipesComponent } from "./components/angular-pipes/angular-pipes.component";
 import { AngularSignalsComponent } from "./components/angular-signals/angular-signals.component";
-import { AngularServicesComponent } from "./components/angular-services/angular-services/angular-services.component";
+import { AngularServicesComponent } from "./components/angular-services/angular-services.component";
 
 @Component({
   selector: 'app-root',
